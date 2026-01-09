78b6400b599ec5b2766cde32ea6a6caede4ca584
Non-existent987 <jiaqi0425_000@163.com>
2026-01-09 23:59:56 +0800

    delete linshi.py

--- tmp_check_import.py
+++
@@ -1,4 +0,0 @@
-import tablegis
-print('module file:', getattr(tablegis, '__file__', None))
-print('has add_polygon:', hasattr(tablegis, 'add_polygon'))
-print('available:', [a for a in dir(tablegis) if 'polygon' in a.lower()])
